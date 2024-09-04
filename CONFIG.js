871ab61d1d385e4aad07265c9d7a9412575bbf68
luongl <leluong.dev@gmail.com>
2024-09-04 15:23:51 +0700

    update

--- CONFIG.js
+++ CONFIG.js
@@ -1,7 +1,7 @@
 const CONFIGDATA = {
   titleHeader: "Chàoo Em, Anh có đôi điều muốn nói. </br> 04-09-2024",
   descriptionHeader:
-    "Em hãy trả lời một cách thật lòng nhé. Nếu Em im lặng và rời đi thì tức là Em đã ĐỒNG Ý heheee.",
+    "Em hãy trả lời một cách thật lòng nhé. Nếu Em im lặng và rời đi thì tức là Em đã ĐỒNG Ý hehee.",
   buttonYes: "Em đồng ý",
   buttonNo: "Khumm :((",
   titleModar: "Cảm ơn Em !.",
